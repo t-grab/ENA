cdb8c613ee70b75844edf752ad8b4d07dd396f05
Xemrox <b.walter94@gmx.de>
2015-01-04 16:26:11 +0100

    Fixed LINT and random crash from activity

--- src/de/hda/ena/praktikum/CategoryAdapter.java
+++ src/de/hda/ena/praktikum/CategoryAdapter.java
@@ -28,15 +28,34 @@ public class CategoryAdapter extends ArrayAdapter<Category> {
 		return this._grouping;
 	}
 
+	private static class ViewHolder {
+	    TextView tvName;
+	    TextView tvValue;
+	}
+	
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		LayoutInflater li = (LayoutInflater) this.getContext().getSystemService(this.getContext().LAYOUT_INFLATER_SERVICE);
-		View rowView = li.inflate(R.layout.categorylistitem, parent, false);
+		View rowView = convertView;
 		
+		 //If the row is null, it means that we aren't recycling anything - so we have
+	    //to inflate the layout ourselves.
+	    ViewHolder holder = null;
+		
+		if(rowView == null) {
+			LayoutInflater li = (LayoutInflater) this.getContext().getSystemService(this.getContext().LAYOUT_INFLATER_SERVICE);
+			rowView = li.inflate(R.layout.categorylistitem, parent, false);
+			holder = new ViewHolder();
+			holder.tvName =  (TextView) rowView.findViewById(R.id.category_name);
+			holder.tvValue =  (TextView) rowView.findViewById(R.id.category_value);
+	        //and store it as the 'tag' of our view
+			rowView.setTag(holder);
+		} else {
+			holder = (ViewHolder) rowView.getTag();
+		}
 		Category c = this.getItem(position);
 		if(c != null) {
-			TextView tvName = (TextView) rowView.findViewById(R.id.category_name);
-	        TextView tvValue = (TextView) rowView.findViewById(R.id.category_value);
+			TextView tvName = holder.tvName;
+	        TextView tvValue = holder.tvValue;
 	        
 	        if(tvName != null) {
 	        	tvName.setText(c.getTitle());
